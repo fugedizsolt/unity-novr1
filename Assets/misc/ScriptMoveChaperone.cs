f1ac26e556bfeebbf1c9d6394c8914c007df545e
fuge <fugedi.zsolt@gmail.com>
2021-01-03 14:28:25 +0100

    gyorsulás kezelés tmp

--- Assets/misc/ScriptMoveChaperone.cs
+++ Assets/misc/ScriptMoveChaperone.cs
@@ -12,6 +12,11 @@ public class ScriptMoveChaperone : MonoBehaviour
     private Vector3 posLeftHandRelativeToChaperoneAtStart;
     private Quaternion quatLeftHandRelativeToChaperoneAtStart;
 
+    private Vector3 currentTranslationAsVelocity;
+    private Vector3 currentTranslationAcceleration;
+    private Vector3 targetTranslationAcceleration;
+    private Quaternion currentRotationAsAngleVelocity;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +24,11 @@ public class ScriptMoveChaperone : MonoBehaviour
         this.posLeftHandRelativeToChaperoneAtStart = this.gameObjLeftHand.transform.localPosition;
         //this.quatLeftHandRelativeToChaperoneAtStart =  Quaternion.Inverse( this.transform.rotation ) * this.gameObjLeftHand.transform.rotation;
         this.quatLeftHandRelativeToChaperoneAtStart = this.gameObjLeftHand.transform.localRotation;
+
+        this.currentTranslationAsVelocity = Vector3.zero;
+        this.currentTranslationAcceleration = Vector3.zero;
+        this.targetTranslationAcceleration = Vector3.zero;
+        this.currentRotationAsAngleVelocity = Quaternion.identity;
     }
 
     // Update is called once per frame
@@ -34,15 +44,20 @@ public class ScriptMoveChaperone : MonoBehaviour
 
         // először kiszámolom a chaperone-hoz viszonyított rotációt
         Quaternion quatTmp1 = this.gameObjLeftHand.transform.localRotation;
+
+        // beállítom a sebességet
+        this.currentRotationAsAngleVelocity = Quaternion.Lerp( this.currentRotationAsAngleVelocity,quatTmp1,deltaTime );
+
         // majd ehhez képest a deltaTime rotiációt
-        Quaternion quatTmp2 = Quaternion.Lerp( Quaternion.identity,quatTmp1,deltaTime );
+        Quaternion quatTmp2 = Quaternion.Lerp( Quaternion.identity,this.currentRotationAsAngleVelocity,deltaTime );
 
         // ezután meg kell határozni azt a chaperone eltolást(transzlációt), amely a hmd-ben történő rotáció miatt éri a chaperone-t
         // el kell menteni a rotáció előtt a hmd pos-t
         Vector3 savedHdmWorldPos = this.gameObjHmd.transform.position;
 
         // végül módosítom a chaperone rotációját, és pozícióját
-        this.transform.rotation *= quatTmp2;
+        Quaternion quatTmp3 = this.transform.rotation * quatTmp2;
+        this.transform.rotation = quatTmp3.normalized;
 
         // kiszámolom az új rotációval a chp-ben a hmd helyzetét és eltolom a chp-t úgy, hogy a hmd 1 helyben kell maradjon
         // ez a két érték egyezik: this.gameObjHmd.transform.position = this.transform.position + this.transform.rotation * this.gameObjHmd.transform.localPosition
@@ -52,10 +67,33 @@ public class ScriptMoveChaperone : MonoBehaviour
         // transzláció kezelés
         // először kiszámolom a chaperone-hoz viszonyított transzlációt, minusz a default érték
         Vector3 diffLeftHandChaperone = this.gameObjLeftHand.transform.localPosition - this.posLeftHandRelativeToChaperoneAtStart;
-        Vector3 diffLeftHandChaperoneLerp = diffLeftHandChaperone * deltaTime;
+        float diffLen = diffLeftHandChaperone.sqrMagnitude;
+        float accLen = Mathf.Pow( diffLen/10f,2f )-1f;
+        this.targetTranslationAcceleration = diffLeftHandChaperone.normalized * accLen;
+        Vector3 diffVelocity = this.targetTranslationAcceleration - this.currentTranslationAcceleration;
+        Vector3 addVelocity = diffVelocity * deltaTime;
+        if ( addVelocity.sqrMagnitude<0.01f )
+            addVelocity = addVelocity.normalized * 0.01f;
+        this.currentTranslationAcceleration += addVelocity;
 
-        Vector3 vecAdd = this.transform.rotation * diffLeftHandChaperoneLerp;
-        this.transform.position += vecAdd;
+        float currentAccelerationVectorLength = this.currentTranslationAcceleration.sqrMagnitude;
+        float targetAccelerationVectorLength = this.targetTranslationAcceleration.sqrMagnitude;
+        if ( currentAccelerationVectorLength>targetAccelerationVectorLength )
+        {
+            this.currentTranslationAcceleration = this.targetTranslationAcceleration;
+            currentAccelerationVectorLength = targetAccelerationVectorLength;
+        }
+
+        if ( currentAccelerationVectorLength>1f )
+        {
+            this.currentTranslationAcceleration = this.currentTranslationAcceleration.normalized * 1f;
+        }
+        this.currentTranslationAsVelocity += this.currentTranslationAcceleration * (deltaTime/10f);
+        this.transform.position += this.currentTranslationAsVelocity * (deltaTime/10f);
+
+        //Vector3 diffLeftHandChaperoneLerp = diffLeftHandChaperone * deltaTime;
+        //Vector3 vecAdd = this.transform.rotation * diffLeftHandChaperoneLerp;
+        //this.transform.position += vecAdd;
     }
 
 	private void updateHUDPosInfo()
@@ -68,6 +106,9 @@ public class ScriptMoveChaperone : MonoBehaviour
         addVector3( "position:{{{0},0:F2}} {{{1},0:F2}} {{{2},0:F2}}\n",this.transform.position );
         addVector3( "posOrigLeftHand:{{{0},0:F6}} {{{1},0:F6}} {{{2},0:F6}}\n",pos1 );
         addVector3( "diff1:{{{0},0:F6}} {{{1},0:F6}} {{{2},0:F6}}\n",pos2 );
+        addVector3( "velocity:{{{0},0:F6}} {{{1},0:F6}} {{{2},0:F6}}\n",this.currentTranslationAsVelocity );
+        addVector3( "currentAcceleration:{{{0},0:F6}} {{{1},0:F6}} {{{2},0:F6}}\n",this.currentTranslationAcceleration );
+        addVector3( "targetAcceleration:{{{0},0:F6}} {{{1},0:F6}} {{{2},0:F6}}\n",this.targetTranslationAcceleration );
 
 		gameObjHUD.text = string.Format( this.strFormat,this.objsFormat );
 	}
